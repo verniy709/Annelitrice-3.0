d04b65c6d380161b8e60af2113bdc8891e76c2e4
verniy709 <81531485+verniy709@users.noreply.github.com>
2023-02-24 11:27:36 -0500

    progress

---
+++ 1.4/Source/Annelitrice/AnnelitriceDefOf.cs
@@ -0,0 +1,56 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Annelitrice
+{
+    [DefOf]
+    public static class AnnelitriceDefOf
+    {
+        public static ThingDef Annelitrice;
+        public static JobDef Anneli_ResurrectUsingEgg;
+
+        //public static HediffDef Anneli_Arachno;
+        //public static HediffDef Anneli_Banelings;
+        //public static HediffDef Anneli_BoneBlade;
+        //public static HediffDef Anneli_BoneSpike;
+        //public static HediffDef Anneli_BoneWing;
+        //public static HediffDef Anneli_Boomalope;
+        //public static HediffDef Anneli_CrushingArm;
+        //public static HediffDef Anneli_DollJoint;
+        //public static HediffDef Anneli_GastrovascularCavity;
+        //public static HediffDef Anneli_Kuchisake;
+        //public static HediffDef Anneli_Lump;
+        //public static HediffDef Anneli_Lumps;
+        //public static HediffDef Anneli_PatagiumWing;
+        //public static HediffDef Anneli_Piercing;
+        //public static HediffDef Anneli_ProliferatingBrain;
+        //public static HediffDef Anneli_Tentacle;
+        //public static HediffDef Anneli_BackTentacle;
+        //public static HediffDef Anneli_ThirdEye;
+        //public static HediffDef Anneli_VenomGland;
+
+        public static BodyPartDef Spine;
+
+        //public static JobDef Anneli_GainEPsFromeggIncubator;
+        //public static JobDef Anneli_InjectEPsToeggIncubator;
+        //public static JobDef Anneli_AssimilateCorpse;
+        public static JobDef Anneli_InfestHuman;
+        public static JobDef Anneli_StandStill;
+        public static HediffDef Anneli_Regeneration;
+        //public static JobDef Anneli_GrowAppendage;
+
+        //public static EffecterDef Anneli_Effecter_SpurtBlood;
+        //public static SoundDef Anneli_Sound_SpurtBlood;
+        public static HediffDef Anneli_Infection;
+        public static PawnKindDef Anneli_Player;
+
+        public static SoundDef Anneli_Sound_AudienceLaugh1;
+        public static SoundDef Anneli_Sound_AudienceLaugh2;
+        public static SoundDef Anneli_Sound_AudienceLaugh3;
+    }
+}
---
+++ 1.4/Source/Annelitrice/AnnelitriceUtils.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Annelitrice
+{
+	public static class AnnelitriceUtils
+	{
+		private static readonly Dictionary<Pawn, CompEvolution> cachedComps = new Dictionary<Pawn, CompEvolution>();
+
+		public static bool TryGetCompEvolution(this Pawn pawn, out CompEvolution comp)
+		{
+			if (!cachedComps.TryGetValue(pawn, out comp))
+			{
+				cachedComps[pawn] = comp = pawn.TryGetComp<CompEvolution>();
+			}
+			return comp != null;
+		}
+
+		//Not sure what this does
+		public static void UpdateGraphics(this Pawn pawn)
+		{
+			pawn.Drawer.renderer.graphics.ResolveAllGraphics();
+			PortraitsCache.SetDirty(pawn);
+			PortraitsCache.PortraitsCacheUpdate();
+			GlobalTextureAtlasManager.TryMarkPawnFrameSetDirty(pawn);
+		}
+
+		//Spawn eggs upon body destruction
+		public static void TrySpawnegg(this Pawn pawn)
+		{
+			var raceExtension = pawn?.def.GetModExtension<RaceExtension>();
+			if (raceExtension != null)
+			{
+				var egg = raceExtension.outsiderSpawnThingAfterDestruction != null && pawn.Faction != Faction.OfPlayer
+					? ThingMaker.MakeThing(raceExtension.outsiderSpawnThingAfterDestruction) as egg
+					: ThingMaker.MakeThing(raceExtension.colonistSpawnThingAfterDestruction) as egg;
+				egg.savedPawn = pawn;
+				GenPlace.TryPlaceThing(egg, pawn.PositionHeld, pawn.MapHeld, ThingPlaceMode.Near);
+			}
+		}
+	}
+}
