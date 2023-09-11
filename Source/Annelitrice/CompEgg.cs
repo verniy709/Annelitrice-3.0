5b9bca235edcc74ddb7e15cbc8ea5de8062611d9
verniy709 <81531485+verniy709@users.noreply.github.com>
2023-09-11 08:06:30 -0400

    20230910_WorkshopRelease

---
+++ Source/Annelitrice/CompEgg.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace Annelitrice
+{
+    public class CompEgg : ThingComp
+    {
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            if (DebugSettings.ShowDevGizmos)
+            {
+                yield return new Command_Action
+                {
+                    defaultLabel = "DEV:Hatch",
+                    action = delegate ()
+                    {
+                        Hatch();
+                    }
+                };
+            }
+        }
+        public override void CompTick()
+        {
+            if (parent.def.defName == "Anneli_Pupa")
+            {
+                eggTime++;
+                if (eggTime > SpawnTime)
+                {
+                    Hatch_Pupa();
+                }
+            }
+        }
+        public void Hatch()
+        {
+            if(parent.def.defName == "Anneli_Pupa")
+            {
+                Hatch_Pupa();
+            }
+            else
+            {
+                Hatch_Egg();
+            }
+        }
+        private void Hatch_Pupa()
+        {
+            IntVec3 pos = parent.Position;
+            Map map = parent.Map;
+            CompContainPawn onwer = parent.GetComp<CompContainPawn>();
+            Pawn pawn = onwer.GetDirectlyHeldThings().First() as Pawn;
+            if (pawn.Dead)
+            {
+                ResurrectionUtility.Resurrect(pawn);
+            }
+            if (pawn.Faction != Faction.OfPlayer)
+            {
+                pawn.SetFaction(Faction.OfPlayer);
+            }
+            onwer.GetDirectlyHeldThings().TryDrop(pawn,pos, map, ThingPlaceMode.Near,1,out _);
+			DefDatabase<EffecterDef>.GetNamed("Anneli_PupationFinish").Spawn(pos, map);
+			parent.Destroy();
+        }
+        private void Hatch_Egg()
+        {
+            IntVec3 pos = parent.Position;
+            Map map = parent.Map;
+            Pawn pawn =PawnGenerator.GeneratePawn(PawnKindDef.Named("AnnelitriceLarvaAsAnimal"),Faction.OfPlayer);
+            parent.GetComp<CompContainPawn>().GetDirectlyHeldThings().TryTransferAllToContainer(pawn.GetComp<CompContainPawn>().GetDirectlyHeldThings());
+            GenSpawn.Spawn(pawn, pos, map, WipeMode.VanishOrMoveAside);
+			parent.Destroy();
+
+        }
+        public override void PostExposeData()
+        {
+            Scribe_Values.Look(ref eggTime, "eggTime");
+		}
+        private int eggTime;
+        public const int SpawnTime = 30000;
+
+		public static ThingWithComps MakeEgg(Pawn pawn)
+        {
+            ThingWithComps egg;
+            
+            if (pawn.Faction != null && pawn.Faction.IsPlayer)
+            {
+                egg = ThingMaker.MakeThing(ThingDef.Named("Anneli_ColonistEgg")) as ThingWithComps;
+            }
+            else
+            {
+                egg = ThingMaker.MakeThing(ThingDef.Named("Anneli_OutsiderEgg")) as ThingWithComps;
+            }
+            egg.TryGetComp<CompContainPawn>().GetDirectlyHeldThings().TryAddOrTransfer(pawn);
+            return egg;
+        }
+		
+		public static ThingWithComps MakeEgg()
+        {
+			Faction AnneliWildFaction = Find.FactionManager.FirstFactionOfDef(DefDatabase<FactionDef>.GetNamed("Anneli_Faction_Wild"));
+			ThingWithComps egg = ThingMaker.MakeThing(ThingDef.Named("Anneli_OutsiderEgg")) as ThingWithComps;
+			Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(PawnKindDef.Named("Anneli_Wild"), AnneliWildFaction, PawnGenerationContext.PlayerStarter, tile: -1, forceGenerateNewPawn: true, allowDead: false, allowDowned: false, canGeneratePawnRelations: false, mustBeCapableOfViolence: false, colonistRelationChanceFactor: 0f, forceAddFreeWarmLayerIfNeeded: false));
+			egg.TryGetComp<CompContainPawn>().GetDirectlyHeldThings().TryAdd(pawn);
+            return egg;
+        }
+    }
+}
