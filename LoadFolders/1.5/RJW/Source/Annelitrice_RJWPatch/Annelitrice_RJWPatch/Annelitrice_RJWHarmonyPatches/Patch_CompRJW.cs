633d674bee66a83b931c74cbd392182cae584198
verniy709 <81531485+verniy709@users.noreply.github.com>
2025-03-25 18:39:53 -0500

    20250325

---
+++ LoadFolders/1.5/RJW/Source/Annelitrice_RJWPatch/Annelitrice_RJWPatch/Annelitrice_RJWHarmonyPatches/Patch_CompRJW.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using AlienRace;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+using rjw;
+using rjw.RenderNodeWorkers;
+
+namespace Annelitrice.RJWHarmonyPatches
+{
+    [HarmonyPatch(typeof(PawnRenderNodeWorker_Apparel_DrawNude), nameof(PawnRenderNodeWorker_Apparel_DrawNude.CanDrawNowSub))]
+    public static class Annelitrice_Render_Patch
+    {
+        public static bool Prefix(PawnRenderNode node, PawnDrawParms parms, ref bool __result)
+        {
+            if (parms.pawn != null && parms.pawn.def.defName == "Annelitrice")
+            {
+                __result = true;
+                return false;      
+            }
+
+            return true;
+        }
+    }
+}
