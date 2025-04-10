ca3613d70d9872e85e24827a1de1500361d4ed38
verniy709 <81531485+verniy709@users.noreply.github.com>
2025-04-10 09:37:39 -0500

    dead face

---
+++ 1.5/Source/Annelitrice/AnnelitriceConditionIsDead.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using RimWorld.Planet;
+using AlienRace;
+using AlienRace.ExtendedGraphics;
+
+namespace Annelitrice
+{
+	public class AnnelitriceConditionIsDead : Condition
+	{
+		public new const string XmlNameParseKey = "AnnelitriceIsDead";
+
+		public bool isDead;
+
+		public override bool Satisfied(ExtendedGraphicsPawnWrapper pawn, ref ResolveData data) =>
+			this.isDead == pawn.WrappedPawn.Dead;
+	}
+}
---
+++ 1.5/Source/Annelitrice/HarmonyPatches/ExtendedGraphicsCondition.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using AlienRace;
+using AlienRace.ExtendedGraphics;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace Annelitrice.HarmonyPatches
+{
+	[HarmonyPatch(typeof(AlienRace.ExtendedGraphics.Condition))]
+	[HarmonyPatch(MethodType.StaticConstructor)]
+	static class ConditionStaticConstructorPatch
+	{
+		static void Postfix()
+		{
+			var xmlNameParseKeys = Traverse.Create(typeof(AlienRace.ExtendedGraphics.Condition))
+				.Field<Dictionary<string, string>>("XmlNameParseKeys").Value;
+			xmlNameParseKeys.Add(AnnelitriceConditionIsDead.XmlNameParseKey, typeof(AnnelitriceConditionIsDead).FullName);
+		}
+	}
+}
