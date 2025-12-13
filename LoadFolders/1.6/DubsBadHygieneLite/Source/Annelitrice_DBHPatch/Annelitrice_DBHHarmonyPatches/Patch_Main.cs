1b8834b1daaf00676596a97991014d46e02737dc
verniy709 <81531485+verniy709@users.noreply.github.com>
2025-12-13 20:27:04 +0800

    revised DBH patch

---
+++ LoadFolders/1.6/DubsBadHygieneLite/Source/Annelitrice_DBHPatch/Annelitrice_DBHHarmonyPatches/Patch_Main.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using System.Reflection;
+using Verse;
+
+namespace Annelitrice.DBHHarmonyPatches
+{
+    [UsedImplicitly]
+    [StaticConstructorOnStartup]
+    public class PatchMain
+    {
+        static PatchMain()
+        {
+            var instance = new Harmony("Annelitrice_RJWHarmonyPatches");
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
