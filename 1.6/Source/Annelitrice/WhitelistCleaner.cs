387846f6319d3854e0a405cacb6f8ce63842e865
verniy709 <verniy79@yahoo.com>
2026-03-03 16:18:46 +0800

    Reapply "white list cleaner"
    This reverts commit 5b42f85eef0bdb38fcf67ec58775e71678823ce7.

---
+++ 1.6/Source/Annelitrice/WhitelistCleaner.cs
@@ -0,0 +1,66 @@
+﻿using Verse;
+using System.Collections.Generic;
+using AlienRace;
+using System.Linq;
+
+namespace Annelitrice
+{
+	[StaticConstructorOnStartup]
+	public static class WhitelistCleaner
+	{
+		static WhitelistCleaner()
+		{
+			ThingDef_AlienRace race = DefDatabase<ThingDef_AlienRace>.GetNamed("Annelitrice");
+
+			if (race != null && race.alienRace?.raceRestriction != null)
+			{
+				var list = race.alienRace.raceRestriction;
+				list.whiteApparelList = new List<ThingDef>();
+
+				AddIfFound(list.whiteApparelList, "Apparel_ShieldBelt");
+				AddIfFound(list.whiteApparelList, "Apparel_SmokepopBelt");
+				AddIfFound(list.whiteApparelList, "Apparel_FirefoampopPack");
+				AddIfFound(list.whiteApparelList, "Apparel_PsychicShockLance");
+				AddIfFound(list.whiteApparelList, "Apparel_PsychicInsanityLance");
+
+				if (ModLister.RoyaltyInstalled)
+				{
+					AddIfFound(list.whiteApparelList, "Apparel_PackJump");
+					AddIfFound(list.whiteApparelList, "Apparel_PackBroadshield");
+				}
+
+				if (ModLister.BiotechInstalled)
+				{
+					AddIfFound(list.whiteApparelList, "Apparel_PackControl");
+					AddIfFound(list.whiteApparelList, "Apparel_PackBandwidth");
+					AddIfFound(list.whiteApparelList, "Apparel_PackTox");
+				}
+
+				if (ModLister.AnomalyInstalled)
+				{
+					AddIfFound(list.whiteApparelList, "Apparel_ShardPsychicShockLance");
+					AddIfFound(list.whiteApparelList, "Apparel_ShardPsychicInsanityLance");
+					AddIfFound(list.whiteApparelList, "Apparel_BiomutationLance");
+					AddIfFound(list.whiteApparelList, "Apparel_DisruptorFlarePack");
+					AddIfFound(list.whiteApparelList, "Apparel_DeadlifePack");
+					AddIfFound(list.whiteApparelList, "Apparel_PackTurret");
+				}
+
+				if (ModLister.OdysseyInstalled)
+				{
+					AddIfFound(list.whiteApparelList, "Apparel_PackHunter");
+					AddIfFound(list.whiteApparelList, "Apparel_CerebrexNode");
+				}
+			}
+		}
+
+		private static void AddIfFound(List<ThingDef> list, string defName)
+		{
+			ThingDef def = DefDatabase<ThingDef>.GetNamedSilentFail(defName);
+			if (def != null)
+			{
+				list.Add(def);
+			}
+		}
+	}
+}
